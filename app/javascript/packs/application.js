2102d07bbae2544395856e1ac7ad06481dbfd5dd
josephlbaker <joelucasbaker@gmail.com>
2019-09-23 16:13:40 -0700

    Installed jquery using webpacker and updated environment js and stylesheet imports so I can have collapsible nav

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("bootstrap/dist/js/bootstrap")
 
 //= require jquery3
 //= require jquery_ujs
