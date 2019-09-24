27bc523bba67fd5ad178d6fd0e106ae32c5e4744
josephlbaker <joelucasbaker@gmail.com>
2019-09-24 14:24:21 -0700

    Installed jquery-ui, sortable html5, css calls --- MAKE SURE JQUERY-UI IS RUNNING AS INTENDED

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,12 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 require("bootstrap/dist/js/bootstrap")
+require("packs/html.sortable.js")
 
 //= require jquery3
+//= require jquery-ui
 //= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets
