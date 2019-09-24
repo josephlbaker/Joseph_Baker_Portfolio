01cdc4ad73c7fa369a0ac1581d2c8bbb455bad16
josephlbaker <joelucasbaker@gmail.com>
2019-09-24 13:20:54 -0700

    Updated portfolio to include position attr

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,7 +4,7 @@ class PortfoliosController < ApplicationController
   layout "portfolio"
 
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
   # DEMONSTRATION REMOVE AT END OF PROJECT
   def angular
