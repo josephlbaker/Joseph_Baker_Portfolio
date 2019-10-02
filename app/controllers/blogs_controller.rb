ec06a0c35e227eaec05c5a830ccb4a8ac680f222
josephlbaker <joelucasbaker@gmail.com>
2019-10-01 18:18:05 -0700

    Implemented redirect for blog show page when user is not an admin

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,8 +18,12 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @page_title = @blog.title
-    @seo_keywords = @blog.title + ", " + @blog.body
+    if logged_in?(:site_admin) || @blog.published?
+      @page_title = @blog.title
+      @seo_keywords = @blog.title + ", " + @blog.body
+    else
+      redirect_to blogs_path, notice: "You are not authorized to view this content"
+    end
   end
 
   # GET /blogs/new
